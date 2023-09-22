dbf588a0ada29dab5a1766f32fdc84484a783db5
tito <107213851+migueltitoxd@users.noreply.github.com>
2023-09-22 17:38:54 -0500

    firstPush

---
+++ js/main.js
@@ -0,0 +1,40 @@
+window.onmousemove = (event) => {
+    document.querySelectorAll('.move').forEach(element => {
+        const data = element.getAttribute('data-value')
+        const x = (window.innerWidth - event.pageX * data) / 80
+        const y = (window.innerHeight - event.pageY * data) / 80
+        element.style.transform = `translateX(${x}px) translateY(${y}px)`
+    })
+}
+
+
+const rocket = document.getElementById('rocket');
+const rocketImage = rocket.querySelector('img');
+
+document.addEventListener('mousemove', (e) => {
+  const rocketRect = rocket.getBoundingClientRect();
+
+  const mouseX = e.clientX;
+  const mouseY = e.clientY;
+
+  const deltaX = (mouseX - rocketRect.left);
+  const deltaY = (mouseY - rocketRect.top);
+
+  const targetRocketX = rocketRect.left + deltaX;
+  const targetRocketY = rocketRect.top + deltaY;
+
+  // Aplicar efecto de amortiguamiento para suavizar el movimiento
+  const rocketX = rocket.offsetLeft + (targetRocketX - rocket.offsetLeft);
+  const rocketY = rocket.offsetTop + (targetRocketY - rocket.offsetTop);
+
+  rocket.style.left = `${rocketX}px`;
+  rocket.style.top = `${rocketY}px`;
+
+  const angle = Math.atan2(mouseY - rocketRect.top, mouseX - rocketRect.left);
+  const rotation = angle * (180 / Math.PI) + 90;
+
+  rocketImage.style.transform = `rotate(${rotation}deg) scaleY(1)`;
+});
+
+
+
